f8e4543e82d15e8b1309eabdda4534120bfa7c3d
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-09 11:04:50 +0300

    updating javascript codes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const myNewTasks = new TaskList();
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+  const taskUnorderedList = document.getElementById("tasks");
+
+  const renderApp = () => (taskUnorderedList.innerHTML = myNewTasks.renderTasks());
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    myNewTasks.createNewTask(taskDescription.value);
+    taskForm.reset(); // Use reset() method to clear the form
+    renderApp();
+  });
+
+  taskUnorderedList.addEventListener("click", (event) => {
+    if (event.target.nodeName === "BUTTON") {
+      myNewTasks.deleteTask(event.target.dataset.description);
+      renderApp();
+    }
+  });
 });
